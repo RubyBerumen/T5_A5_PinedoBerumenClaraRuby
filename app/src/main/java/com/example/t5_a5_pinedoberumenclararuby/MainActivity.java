88661c9dac7bd2b80b007d9af3e7a6813091cc82
Ruby Berumen <clararubipb15@gmail.com>
2021-05-28 21:38:53 -0500

    Se creó la interfáz gráfica

---
+++ app/src/main/java/com/example/t5_a5_pinedoberumenclararuby/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.t5_a5_pinedoberumenclararuby;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
